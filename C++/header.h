e2994b5a8548adee551c179933ba2a01e59c2854
stevensikorski <ssikorski03@gmail.com>
2022-08-17 20:11:58 -0400

    Added header.h

---
+++ C++/header.h
@@ -0,0 +1,12 @@
+/*
+
+@file       header.h
+@author     stevensikorski
+@date       August 17 2022
+@brief      Header file for LeetCode problmes using C++
+
+*/
+
+#include <iostream>
+
+using namespace std;
