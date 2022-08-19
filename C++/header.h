0d6129062092445ab9acaca57dadb018c54e7f8f
stevensikorski <ssikorski03@gmail.com>
2022-08-19 12:53:40 -0400

    Updated header.h

--- C++/header.h
+++ C++/header.h
@@ -3,10 +3,18 @@
 @file       header.h
 @author     stevensikorski
 @date       August 17 2022
-@brief      Header file for LeetCode problmes using C++
+@brief      Header file
 
 */
 
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <vector>
 
 using namespace std;
