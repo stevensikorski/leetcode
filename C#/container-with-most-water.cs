4d5ab85c8ff46843cb676f6549503bd6773d22fd
stevensikorski <ssikorski03@gmail.com>
2022-12-28 00:56:50 -0500

    Updated container-with-most-water.cs

--- C#/container-with-most-water.cs
+++ C#/container-with-most-water.cs
@@ -14,14 +14,8 @@ public class Solution {
             int area = (right - left) * Math.Min(height[left], height[right]); // We calculate the gap * minimum of both heights
             max = Math.Max(max, area); // Get the maximum
 
-            if (height[left] < height[right]) // If left is smaller, bring it closer to the middle
-            {
-                left++;
-            }
-            else // Else (if right is smaller or equal to left), bring it closer to the middle
-            {
-                right--;
-            }
+            if (height[left] < height[right]) left++; // If left is smaller, bring it closer to the middle
+            else right--; // Else (if right is smaller or equal to left), bring it closer to the middle
         }
 
         return max;
